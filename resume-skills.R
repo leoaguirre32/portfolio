9edf317590e2f5dd4254ba1112512e77d5d68af2
leoaguirre32 <leoaguirre32@gmail.com>
2015-04-28 13:45:01 -0300

    Create resume-skills.R

---
+++ resume-skills.R
@@ -0,0 +1,19 @@
+require(ggplot2)
+
+# create data
+skills <- data.frame(dominio=c(1,2,3,4), 
+                     linguagem=factor(
+                       c("XML, PMML,\nAngularJS, SPSS", 
+                         "HTML, CSS,\nJavascript, iMacros", 
+                         "SQL, Python", 
+                         "R, SAS")))
+
+# reorder factor levels
+skills$linguagem <- factor(skills$linguagem, levels(skills$linguagem)[c(4,1,3,2)])
+
+# create labels
+labels <- c("", "I've been there", "Confortable", "Very\nconfortable", "Expert")
+
+# plot data
+ggplot(skills, aes(linguagem, dominio)) + geom_bar(stat="identity", fill="#428BCA") + coord_flip() +
+  scale_y_discrete(labels=labels) + xlab("") + ylab("")
